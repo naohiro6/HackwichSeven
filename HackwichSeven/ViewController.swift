1d2617e81ff18102cd15b88891b25a9fb947437e
CM Student <cmstudent@UHWO-ACM-18.local>
2019-04-04 09:57:19 -1000

    commit

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude: 21.361888 , longitude: -158.055725)
+    
+    let regionRadius: CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        centerMapOnLocation(location: initialLocation)
+        
+    }
+    
+    func centerMapOnLocation(location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
